127cf378a7d5d0bd5bdc6629a5721c7ca47f7df9
UMU618 <umu618@hotmail.com>
2019-11-29 14:54:23 +0800

    + key pair with new format only

---
+++ gen-new-key-pair.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/**
+ * @author UMU618 <umu618@hotmail.com>
+ * @copyright MEET.ONE 2019
+ * @description Use block-always-using-brace npm-coding-style.
+ */
+
+'use strict'
+
+const keyUtil = require('./key-util')
+
+const bsc = require('bs58check')
+const EC = require('elliptic').ec
+const k1 = new EC('secp256k1')
+const key = k1.genKeyPair()
+
+const pri = key.getPrivate()
+console.log(pri)
+const prikey = keyUtil.keyToString('PVT_K1_', pri.toBuffer(), 'K1')
+console.log(prikey)
+
+const pub = key.getPublic()
+const y = pub.getY().isEven() ? 2 : 3
+const pubkey = keyUtil.keyToString('PUB_K1_'
+  , Buffer.from([y].concat(pub.getX().toArray())), 'K1')
+console.log(pubkey)
